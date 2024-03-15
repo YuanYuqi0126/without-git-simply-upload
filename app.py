f5e551c3a3208d134274e8120bddc9d90b17f7d0
YuanYuqi0126 <142371424+YuanYuqi0126@users.noreply.github.com>
2024-03-15 19:06:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -35,16 +30,3 @@ def end():
                
 if __name__ == "__main__":
     app.run()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
